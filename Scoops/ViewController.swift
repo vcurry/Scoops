2f2359249b3e8fedf6a3f8b9b6ec815924d771ba
Verónica Cordobés <verocordobes@hotmail.com>
2016-10-20 15:55:45 +0200

    Commit inicial

---
+++ Scoops/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scoops
+//
+//  Created by Verónica Cordobés on 20/10/16.
+//  Copyright © 2016 Verónica Cordobés. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
